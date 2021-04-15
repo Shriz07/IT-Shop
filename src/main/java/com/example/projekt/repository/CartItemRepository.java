9095257eb79691a1c5838351bc7143e3411f7e71
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-15 18:03:05 +0200

    Added method to get cart items by user

--- src/main/java/com/example/projekt/repository/CartItemRepository.java
+++ src/main/java/com/example/projekt/repository/CartItemRepository.java
@@ -1,8 +1,12 @@
 package com.example.projekt.repository;
 
 import com.example.projekt.model.CartItem;
+import com.example.projekt.model.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface CartItemRepository extends CrudRepository<CartItem, Integer>
 {
+    public List<CartItem> findByUser(User user);
 }
