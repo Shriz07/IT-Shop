0c8c5c4c08135276941d08a411707e747b052a1e
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-15 18:09:14 +0200

    Added cart item serice interface

---
+++ src/main/java/com/example/projekt/service/ICartItemService.java
@@ -0,0 +1,10 @@
+package com.example.projekt.service;
+
+import com.example.projekt.model.CartItem;
+
+import java.util.List;
+
+public interface ICartItemService
+{
+    public List<CartItem> findAll();
+}
