f2826a5111b477c33a30ff3ae5ef0916c257c99d
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-17 15:07:47 +0200

    Added payment method repository

---
+++ src/main/java/com/example/projekt/repository/PaymentMethodRepository.java
@@ -0,0 +1,9 @@
+package com.example.projekt.repository;
+
+import com.example.projekt.model.PaymentMethod;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PaymentMethodRepository extends CrudRepository<PaymentMethod, Integer>
+{
+
+}
