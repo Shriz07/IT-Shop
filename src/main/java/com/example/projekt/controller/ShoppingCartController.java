a94ed7b00bc68a892c952ac305c4988cb15b6e1a
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-17 15:15:21 +0200

    Removed unused service

--- src/main/java/com/example/projekt/controller/ShoppingCartController.java
+++ src/main/java/com/example/projekt/controller/ShoppingCartController.java
@@ -4,11 +4,8 @@ import com.example.projekt.details.CustomUserDetails;
 import com.example.projekt.model.CartItem;
 import com.example.projekt.model.User;
 import com.example.projekt.service.CartItemService;
-import com.example.projekt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +17,6 @@ public class ShoppingCartController
 {
     @Autowired
     private CartItemService cartItemService;
-
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/shoppingCart")
     public String cart(Model model, @AuthenticationPrincipal CustomUserDetails customUserDetails)
