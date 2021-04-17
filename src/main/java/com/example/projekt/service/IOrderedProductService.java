f2ba6b15fb8e76d8c58fe4987a8fbcca613d3a63
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-17 15:04:01 +0200

    Added ordered product service interface

---
+++ src/main/java/com/example/projekt/service/IOrderedProductService.java
@@ -0,0 +1,10 @@
+package com.example.projekt.service;
+
+import com.example.projekt.model.OrderedProduct;
+
+import java.util.List;
+
+public interface IOrderedProductService
+{
+    public List<OrderedProduct> findAll();
+}
