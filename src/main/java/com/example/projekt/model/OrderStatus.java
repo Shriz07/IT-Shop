1d23904f4e44e2b61c461939d48af1d06e0f48d6
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-23 12:41:02 +0200

    Added order status entity

---
+++ src/main/java/com/example/projekt/model/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.example.projekt.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_statuses")
+public class OrderStatus
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer statusId;
+
+    private String name;
+
+    public Integer getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Integer statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
