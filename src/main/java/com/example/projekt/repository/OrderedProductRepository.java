3baa1c051b12ff62afdf0f4357e93d2f7518f68b
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-24 14:28:21 +0200

    Added method to get ordered products by order

--- src/main/java/com/example/projekt/repository/OrderedProductRepository.java
+++ src/main/java/com/example/projekt/repository/OrderedProductRepository.java
@@ -1,9 +1,12 @@
 package com.example.projekt.repository;
 
+import com.example.projekt.model.Order;
 import com.example.projekt.model.OrderedProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderedProductRepository extends JpaRepository<OrderedProduct, Integer>
 {
-
+    public List<OrderedProduct> findByOrder(Order order);
 }
--- src/main/java/com/example/projekt/service/OrderedProductService.java
+++ src/main/java/com/example/projekt/service/OrderedProductService.java
@@ -1,11 +1,14 @@
 package com.example.projekt.service;
 
+import com.example.projekt.model.Order;
 import com.example.projekt.model.OrderedProduct;
 import com.example.projekt.repository.OrderedProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class OrderedProductService implements IOrderedProductService
 {
     @Autowired
@@ -14,5 +17,10 @@ public class OrderedProductService implements IOrderedProductService
     @Override
     public List<OrderedProduct> findAll() {
         return (List<OrderedProduct>) orderedProductRepository.findAll();
+    }
+
+    public List<OrderedProduct> findByOrder(Order order)
+    {
+        return orderedProductRepository.findByOrder(order);
     }
 }
