786dcbe9554aba3ba50e94df87225f641b31172e
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-26 10:58:48 +0200

    Added method to find orders by user

--- src/main/java/com/example/projekt/repository/OrderRepository.java
+++ src/main/java/com/example/projekt/repository/OrderRepository.java
@@ -1,13 +1,18 @@
 package com.example.projekt.repository;
 
 import com.example.projekt.model.Order;
+import com.example.projekt.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer>
 {
     @Query("UPDATE Order o SET o.orderStatus.statusId = ?2 WHERE o.orderId = ?1")
     @Modifying
     public void updateOrderStatus(Integer orderId, Integer orderStatusId);
+
+    public List<Order> findByUser(User user);
 }
