63f540ec6d2bf4e17fe6c2e834a3cb6c26fa2713
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-29 13:03:02 +0200

    Removed unused import

--- src/main/java/com/example/projekt/repository/ProductRepository.java
+++ src/main/java/com/example/projekt/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.example.projekt.repository;
 import com.example.projekt.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
