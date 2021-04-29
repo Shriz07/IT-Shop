5e441e992dbaa0005a7ba81aced6d0707a2f88b0
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-29 13:02:31 +0200

    Added method to add payment

--- src/main/java/com/example/projekt/service/PaymentService.java
+++ src/main/java/com/example/projekt/service/PaymentService.java
@@ -1,11 +1,14 @@
 package com.example.projekt.service;
 
 import com.example.projekt.model.Payment;
+import com.example.projekt.model.PaymentMethod;
 import com.example.projekt.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PaymentService implements IPaymentService
 {
     @Autowired
@@ -14,5 +17,15 @@ public class PaymentService implements IPaymentService
     @Override
     public List<Payment> findAll() {
         return (List<Payment>) paymentRepository.findAll();
+    }
+
+    public Payment addPayment(float total, PaymentMethod paymentMethod, boolean isPaid)
+    {
+        Payment payment = new Payment();
+        payment.setTotalAmount(total);
+        payment.setPaymentMethod(paymentMethod);
+        payment.setPaid(true);
+        paymentRepository.save(payment);
+        return payment;
     }
 }
