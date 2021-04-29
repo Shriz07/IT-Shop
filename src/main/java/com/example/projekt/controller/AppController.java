5ed0e9454466955ebdcd269eaf21759598275117
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-29 12:42:59 +0200

    Moved register user to user controller

--- src/main/java/com/example/projekt/controller/AppController.java
+++ src/main/java/com/example/projekt/controller/AppController.java
@@ -19,17 +19,12 @@ import java.util.List;
 @Controller
 public class AppController
 {
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private ProductService productService;
 
     @Autowired
     private CategoryService categoryService;
-
-    @Autowired
-    private AddressRepository addressRepository;
 
     @Autowired
     private CartItemService cartItemService;
@@ -54,30 +49,6 @@ public class AppController
         model.addAttribute("user", new User());
         model.addAttribute("address", new Address());
         return "register";
-    }
-
-    @PostMapping("/registerUser")
-    public String registerUser(Model model, User user, Address address)
-    {
-        if( userRepository.findByEmail(user.getEmail()) != null)
-        {
-            model.addAttribute("user", new User());
-            model.addAttribute("address", new Address());
-            model.addAttribute("status", "User with that email exists.");
-            return "register";
-        }
-        Address addr = addressRepository.findAddressByCityAndPostalCodeAndStreetAndHomeNumber(address.getCity(), address.getPostalCode(), address.getStreet(), address.getHomeNumber());
-        if(addr != null)
-            address = addr;
-        else
-            addressRepository.save(address);
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encodedPassword = encoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-        user.setRole("user");
-        user.setAddressId(address.getAddressId());
-        userRepository.save(user);
-        return "registerSuccess";
     }
 
     @GetMapping("/contactUs")
