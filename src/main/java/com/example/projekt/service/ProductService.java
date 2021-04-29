6714c1e367a6f64e2c920f2a16137b1e481a8645
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-29 13:03:25 +0200

    Added crud methods

--- src/main/java/com/example/projekt/service/ProductService.java
+++ src/main/java/com/example/projekt/service/ProductService.java
@@ -2,23 +2,47 @@ package com.example.projekt.service;
 import com.example.projekt.model.Product;
 import com.example.projekt.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class ProductService implements IProductService
 {
     @Autowired
-    private ProductRepository repository;
+    private ProductRepository productRepository;
 
     @Override
     public List<Product> findAll() {
-        return (List<Product>) repository.findAll();
+        return (List<Product>) productRepository.findAll();
     }
 
     @Override
     public List<Product> findAllInCategory(Integer id) {
-        return repository.findAllInCategory(id);
+        return productRepository.findAllInCategory(id);
+    }
+
+    public List<Product> findAllWithSpecs(Specification<Product> specs) { return productRepository.findAll(Specification.where(specs)); }
+
+    public Product findById(Integer id) { return productRepository.findById(id).get(); }
+
+    public void deleteById(Integer id) { productRepository.deleteById(id); }
+
+    public void addProduct(Product product, String filename, Integer brandId, Integer categoryId)
+    {
+        product.setImage(filename);
+        product.setBrandId(brandId);
+        product.setCategoryId(categoryId);
+        productRepository.save(product);
+    }
+
+    public void updateProduct(Product product, Integer categoryId, Integer brandId)
+    {
+        product.setCategoryId(categoryId);
+        product.setBrandId(brandId);
+        productRepository.save(product);
     }
 }
