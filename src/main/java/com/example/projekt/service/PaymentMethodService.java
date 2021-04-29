1fc801dcf0aa559593d38d83514acb2a745591a2
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-04-29 13:02:19 +0200

    Added method to find payment

--- src/main/java/com/example/projekt/service/PaymentMethodService.java
+++ src/main/java/com/example/projekt/service/PaymentMethodService.java
@@ -3,9 +3,11 @@ package com.example.projekt.service;
 import com.example.projekt.model.PaymentMethod;
 import com.example.projekt.repository.PaymentMethodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PaymentMethodService implements IPaymentMethodService
 {
     @Autowired
@@ -15,4 +17,6 @@ public class PaymentMethodService implements IPaymentMethodService
     public List<PaymentMethod> findAll() {
         return (List<PaymentMethod>) paymentMethodRepository.findAll();
     }
+
+    public PaymentMethod findById(Integer id) { return paymentMethodRepository.findById(id).get(); }
 }
