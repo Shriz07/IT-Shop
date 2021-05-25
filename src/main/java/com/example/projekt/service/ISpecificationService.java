f7f73a8df2bcee3d5c9745132dbba1fe9c6fd8dc
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-05-25 20:03:16 +0200

    Added specification service interface

---
+++ src/main/java/com/example/projekt/service/ISpecificationService.java
@@ -0,0 +1,10 @@
+package com.example.projekt.service;
+
+import com.example.projekt.model.Specification;
+
+import java.util.List;
+
+public interface ISpecificationService
+{
+    public List<Specification> findAll();
+}
