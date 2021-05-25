964c083866840b6aae0605685955c325c5275036
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-05-25 20:02:46 +0200

    Added specification controller

---
+++ src/main/java/com/example/projekt/controller/SpecificationController.java
@@ -0,0 +1,38 @@
+package com.example.projekt.controller;
+
+import com.example.projekt.model.Specification;
+import com.example.projekt.service.SpecificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class SpecificationController
+{
+    @Autowired
+    SpecificationService specificationService;
+
+    @GetMapping("/addSpecification")
+    public String addSpecification(Model model, @RequestParam(value = "productId") Integer productId)
+    {
+        Specification specification = new Specification();
+        model.addAttribute("specification", specification);
+        model.addAttribute("productId", productId);
+        return "addSpecification";
+    }
+
+    @PostMapping("/addSpecification")
+    public String addSpecification(Model model, Specification specification, @RequestParam(value = "productId") Integer productId)
+    {
+        specification.setProductId(productId);
+        specificationService.addSpecification(specification);
+        model.addAttribute("status", "Specification added");
+        model.addAttribute("specification", new Specification());
+        model.addAttribute("productId", productId);
+        return "addSpecification";
+    }
+}
