507913ff4de16093cbddf458013b14640ad47c6a
Adam Mąkiewicz <56472765+Shriz07@users.noreply.github.com>
2021-05-25 17:58:50 +0200

    Button "Buy now" is now working

--- src/main/resources/static/js/add_to_cart.js
+++ src/main/resources/static/js/add_to_cart.js
@@ -2,12 +2,34 @@ $(document).ready(function (){
     $("#buttonAddToCart").on("click", function (){
        addToCart();
     });
+    $("#buttonBuyNow").on("click", function (){
+        buyNow();
+    });
 });
+
+function buyNow()
+{
+    let quantity = $("#quantity" + productId).val();
+    let url = "/cart/add/" + productId + "/" + quantity;
+    $.ajax({
+        type: "POST",
+        url: url,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader(csrfHeaderName, csrfValue);
+        }
+    }).done(function (response) {
+        window.location = "/shoppingCart";
+    }).fail(function () {
+        $("#modalTitle").text("Shopping cart");
+        $("#modalBody").text("Error while adding product to cart.");
+        $("#myModal").modal();
+    });
+}
 
 function addToCart()
 {
-    quantity = $("#quantity" + productId).val();
-    url = "/cart/add/" + productId + "/" + quantity;
+    let quantity = $("#quantity" + productId).val();
+    let url = "/cart/add/" + productId + "/" + quantity;
     $.ajax({
        type: "POST",
        url: url,
